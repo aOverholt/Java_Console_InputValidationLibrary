920c24c04995e480f310e08a179419cb6dbe3803
Andrew <Andrew@andrews-mbp>
2020-12-15 00:10:22 -0500

    - Created test for getSpecificInt(String prompt, int firstInt)

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,7 @@
 package test;
 
 import static validation.InputValidation.*;
+
 public class Test
 {
     public static void main(String[] args)
@@ -12,28 +13,24 @@ public class Test
         
         // getInt(String prompt)
         aInt = getInt("Enter an Integer: ");
-        System.out.println("Results of getInt(): " + aInt + "\n");
+        System.out.println("getInt(String prompt): PASSED!\n");
         
         // getInt(String prompt, int minInt, int maxInt)
         aInt = getInt("Enter an Integer between 3 and 14: ", 3, 14);
-        System.out.println("Results of getInt(): " + aInt + "\n");
-/*
+        System.out.println("getInt(String prompt, int minInt, int maxInt): PASSED!\n");
+        
         // getSpecificInt(String prompt, int firstInt)
-        aInt = getInt("Enter an Integer: ");
-        System.out.println("Results of getInt(): " + aInt);
+        aInt = getSpecificInt("Enter an Integer(1): ", 1);
+        System.out.println("getSpecificInt(String prompt, int firstInt): PASSED!\n");
+/*
     
         // getSpecificInt(String prompt, int firstInt, int secondInt) 
-        aInt = getInt("Enter an Integer: ");
-        System.out.println("Results of getInt(): " + aInt);
 
         // getSpecificInt(String prompt, int firstInt, int secondInt, int thirdInt) 
-        aInt = getInt("Enter an Integer: ");
-        System.out.println("Results of getInt(): " + aInt);
 
         // getSpecificInt(String prompt, int firstInt, int secondInt, int thirdInt, int fourthInt) 
-        aInt = getInt("Enter an Integer: ");
-        System.out.println("Results of getInt(): " + aInt);
         
         */
     }
+
 }
